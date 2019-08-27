1ed596f17552b4de61c4ce85f8223ffee352ec0a
thanhdn <thanhdn@opentechiz.com>
2019-08-27 10:40:18 +0700

    update

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,12 +1,15 @@
-const express = require("express");
+const imageServices = require("image.services");
 
 async function startServer() {
-  const app = express();
-  app.listen(5000, () => {
-    console.log("#################################################");
-    console.log(" 🛡️  worker listening on port: ", 5000, " 🛡️ ");
-    console.log("#################################################");
-  });
+  const url =
+    "https:////mktimg2.com/2/3266/__AMykjN2IzM/__AO4YTMykTMa7f65e01aa326cebbaad4c69f83083257a0aeacee3cb822049b13e8ac4fac43685329.jpg";
+  const hash =
+    "32810e9669e8d5e8ea9a86d5e48afa1c22ce2ab45db079c383e5e5c017e1c845";
+
+  imageServices
+    .saveImageFromUrl({ url, hash })
+    .then(() => console.log("Save image Success with hash: %s", hash))
+    .catch(console.log);
 }
 
 startServer();
